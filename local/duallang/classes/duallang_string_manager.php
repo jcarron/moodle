e18daccf94b13346979c5465acce9b2faf1a89cb
Jeff Carron <jeffrey.carron@gmail.com>
2020-08-13 22:32:05 -0400

    added settings page - chap 3

--- local/duallang/classes/duallang_string_manager.php
+++ local/duallang/classes/duallang_string_manager.php
@@ -8,18 +8,18 @@
 //
 // Moodle is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @package     local_duallang
- * @copyright   2016 Ian Wild - based on local_stringman by David Mudrak <david@moodle.com>
- * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ * @package local_duallang
+ * @copyright 2016 Ian Wild - based on local_stringman by David Mudrak <david@moodle.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
 namespace local_duallang;
 
 defined('MOODLE_INTERNAL') || die();
@@ -27,31 +27,45 @@ defined('MOODLE_INTERNAL') || die();
 /**
  * Override the get_string() function to display two language strings, in this instance UK English and simplified Chinese Mandarin.
  *
- * Based on local_stringman by David Mudrak <david@moodle.com> 
+ * Based on local_stringman by David Mudrak <david@moodle.com>
+ *
  * @copyright 2015 David Mudrak <david@moodle.com>
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class duallang_string_manager extends \core_string_manager_standard {
+class duallang_string_manager extends \core_string_manager_standard
+{
 
     /**
      * Implementation of the get_string() method to display both simplified Chinese and UK English simultaneously.
      *
-     * @param string $identifier the identifier of the string to search for
-     * @param string $component the component the string is provided by
-     * @param string|object|array $a optional data placeholder
-     * @param string $lang moodle translation language, null means use current
+     * @param string $identifier
+     *            the identifier of the string to search for
+     * @param string $component
+     *            the component the string is provided by
+     * @param string|object|array $a
+     *            optional data placeholder
+     * @param string $lang
+     *            moodle translation language, null means use current
      * @return string
      */
-    public function get_string($identifier, $component = '', $a = null, $lang = null) {
-        
-        $string = parent::get_string($identifier, $component, $a, 'en');
-        
-        $zh_cn = parent::get_string($identifier, $component, $a, 'zh_cn');
-         
-        if(strlen($zh_cn) > 0) {
-            $string .= ' | ' . $zh_cn;
+    public function get_string($identifier, $component = '', $a = null, $lang = null)
+    {
+        $string = ''; // returns an empty string by default
+        if (get_config('local_duallang', 'enabled') == "1") {
+            $order = get_config('local_duallang', 'readingorder');
+            $primarylang = parent::get_string($identifier, $component, $a, get_config('local_duallang', 'primarylanguage'));
+            $secondarylang = parent::get_string($identifier, $component, $a, get_config('local_duallang', 'secondarylanguage'));
+            // ordering is only left to right or right to left so just check the first letter of the string
+            if ($order[0] == 'L') {
+                $string = $primarylang . ' (' . $secondarylang . ')';
+            } else {
+                $string = '(' . $secondarylang . ') ' . $primarylang;
+            }
         }
-        
+        else {
+            $string = parent::get_string($identifier, $component, $a, $lang);
+        }
+
         return $string;
     }
 }
